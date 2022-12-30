533b334797b7eb4cc01ba1b62bd99e9ef03b2477
Prajuk Nusbaum <deomax@yahoo.com>
2022-12-30 13:26:39 -0600

    just a date no time

--- DateOnly/Program.cs
+++ DateOnly/Program.cs
@@ -1,6 +1,12 @@
 ﻿// Date Only Variables
 // only in .NET 6
 
+DateTime today= DateTime.Now;
+
 DateOnly birthday = DateOnly.Parse(s: "8/20/1969");
 
 Console.WriteLine(birthday.ToString(format: "MMM dd, yyyy"));
+
+Console.WriteLine(value: $"Today full format: {today}");
+Console.WriteLine(value: $"Today just date: {today.Date}");
+Console.WriteLine(value: $"Birthday full format: {birthday.ToString()}");
